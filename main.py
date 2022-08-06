f732ce83a98df47d62ea02196aeb57672d272e72
rperry2801 <62030975+rperry2801@users.noreply.github.com>
2022-08-06 15:18:37 -0400

    Create the program main

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+class RunMain:
+  def __init__(self):
+    print("Program Main")
+
+Main = RunMain()
+Window = Tk()
+Window.mainloop()
