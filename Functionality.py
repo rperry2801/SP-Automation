f95afa3e5f65135ed8f47a26df5515c463150559
rperry2801 <62030975+rperry2801@users.noreply.github.com>
2022-08-07 15:24:51 -0400

    Update Functionality.py

--- Functionality.py
+++ Functionality.py
@@ -1,17 +1,15 @@
-# Install FPDF via pip: "py -m pip install FPDF"
+# Install FPDF via pip "py -m pip install FPDF"
 import fpdf
 from fpdf import FPDF
 
-# First all the data we write needs to go to a text file we create that here
-text = open("WriteTo.txt", "a")
-text.write("This is where all the data will go!")
-text.close()
-
 class PDF(FPDF):
-  def positionText(self):
-    # self.cell()
-    # whatever code is needed to ensure text will be placed in the right part of the form
+  def __init__(self):
+    print("Testing PDF Writing Options")
+    self.data = input("Write to PDF> ")
+    self.writeTextCell()
+  def writeTextCell(self, x, y):
+    self.cell(x, y, "Hello World!", 1)
+  def writeUseWrite(self, loc, user_data):
+    self.write(loc, user_data)
 
-print(text.read())
 pdf = PDF()
-pdf.positionText()
