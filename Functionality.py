cd544eb6718ccea6aa1fa28715282159911d730a
rperry2801 <62030975+rperry2801@users.noreply.github.com>
2022-08-09 12:52:00 -0400

    Update Functionality.py

--- Functionality.py
+++ Functionality.py
@@ -6,10 +6,11 @@ class PDF(FPDF):
   def __init__(self):
     print("Testing PDF Writing Options")
     self.data = input("Write to PDF> ")
-    self.writeTextCell(10,10)
-  def writeTextCell(self, x, y):
-    self.cell(x, y, "Hello World!", 1)
-  def writeUseWrite(self, loc, user_data):
+    self.writeTextCell(10,10, self.data)
+   
+  def writeTextCell(self, x, y, user_input):
+    self.cell(x, y, user_input, 1)
+  def writeUsingWrite(self, loc, user_data):
     self.write(loc, user_data)
 
 pdf = PDF()
